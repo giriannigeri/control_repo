288bfaa97ce7e349df2b357a503ae852300b5535
giriannigeri <128058181+giriannigeri@users.noreply.github.com>
2023-03-20 16:28:32 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,3 @@
 node default {
-  include role::app_server
-}
-
-node 'puppetmaster.domain.name' {
-  include role::master_Server
-}
-
-node /^web/ {
-  include role::app_server
-}
-
-node /^db/ {
-  include role::db_server
+  include role::ntp_server
 }
